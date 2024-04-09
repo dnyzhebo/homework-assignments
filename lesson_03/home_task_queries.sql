42e727498b34d37ec5954539e6f92f47cffda97b
dnyzhebo <dmytronyzheborets@gmail.com>
2024-04-10 00:48:26 +0300

    First task done

---
+++ lesson_03/home_task_queries.sql
@@ -0,0 +1,62 @@
+/*
+ Завдання на SQL до лекції 03.
+ */
+
+
+/*
+1.
+Вивести кількість фільмів в кожній категорії.
+Результат відсортувати за спаданням.
+*/
+-- SQL code goes here...
+
+SELECT
+    COUNT(fc.FILM_ID) AS qty_films,
+    ctg.NAME
+FROM
+    PUBLIC.FILM_CATEGORY fc
+INNER JOIN
+    PUBLIC.CATEGORY ctg ON fc.CATEGORY_ID = ctg.CATEGORY_ID
+GROUP BY
+    ctg.NAME
+ORDER BY
+    qty_films DESC;
+
+
+
+/*
+2.
+Вивести 10 акторів, чиї фільми брали на прокат найбільше.
+Результат відсортувати за спаданням.
+*/
+-- SQL code goes here...
+
+
+
+SELECT *
+FROM PUBLIC.FILM_ACTOR
+ORDER BY 2 DESC
+LIMIT 10;
+
+/*
+3.
+Вивести категорія фільмів, на яку було витрачено найбільше грошей
+в прокаті
+*/
+-- SQL code goes here...
+
+
+
+/*
+4.
+Вивести назви фільмів, яких не має в inventory.
+Запит має бути без оператора IN
+*/
+-- SQL code goes here...
+
+
+/*
+5.
+Вивести топ 3 актори, які найбільше зʼявлялись в категорії фільмів “Children”.
+*/
+-- SQL code goes here...
